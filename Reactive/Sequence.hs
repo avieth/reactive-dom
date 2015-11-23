e577d8c07a30f40958e57d41b1c5eca0badf9a55
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-11-23 03:43:23 -0500

    Lazier Sequence

--- Reactive/Sequence.hs
+++ Reactive/Sequence.hs
@@ -10,17 +10,19 @@ Portability : non-portable (GHC only)
 
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GADTs #-}
 
 module Reactive.Sequence (
 
       Sequence(..)
+    , (||>)
     , (|>)
+    , always
     , sequenceFirst
     , sequenceRest
     , sequenceOnce
     , sequenceBehavior
     , bundle
-    , always
     , fromEvent
     , toEvent
     , toBehavior
@@ -42,6 +44,7 @@ module Reactive.Sequence (
 
     ) where
 
+import Control.Monad
 import Control.Monad.IO.Class
 import Data.EitherBoth
 import Data.Semigroup
@@ -49,32 +52,41 @@ import Data.Monoid hiding ((<>))
 import Reactive.Banana.Combinators
 import Reactive.Banana.Frameworks
 
-data Sequence t = Sequence (MomentIO (t, Event t))
+import Debug.Trace
+
+data Sequence t where
+    Sequence :: MomentIO (s, MomentIO (Event s)) -> (s -> t) -> Sequence t
 
 instance Functor Sequence where
-    fmap f ~(Sequence m) = Sequence (fmap f' m)
-      where
-        f' ~(t, ev) = (f t, fmap f ev)
+    fmap f (Sequence m g) = Sequence m (fmap f g)
 
 instance Applicative Sequence where
     pure = sequenceOnce
     mf <*> mx = (uncurry ($)) <$> (bundle mf mx)
 
-x |> y = Sequence (pure (x, y))
+(||>) :: MomentIO t -> MomentIO (Event t) -> Sequence t
+x ||> y = Sequence ((\x -> (x, y)) <$> x) id
+
+(|>) :: t -> Event t -> Sequence t
+x |> y = Sequence (pure (x, pure y)) id
+
+always :: t -> Sequence t
+always x = x |> never
 
 sequenceFirst :: Sequence t -> MomentIO t
-sequenceFirst ~(Sequence m) = fst <$> m
+sequenceFirst (Sequence m f) = (f . fst) <$> m
 
 sequenceRest :: Sequence t -> MomentIO (Event t)
-sequenceRest ~(Sequence m) = snd <$> m
+sequenceRest (Sequence m f) = (fmap . fmap) f (join (snd <$> m))
 
 sequenceOnce :: t -> Sequence t
-sequenceOnce x = Sequence (pure (x, never))
+sequenceOnce = always
 
 sequenceBehavior :: Sequence t -> MomentIO (Behavior t)
-sequenceBehavior ~(Sequence m) = do
-    (t, ev) <- m
-    stepper t ev
+sequenceBehavior (Sequence m f) = do
+    (t, mkEv) <- m
+    ev <- mkEv
+    stepper (f t) (f <$> ev)
 
 -- | This is the applicative <*> in another form.
 bundle
@@ -82,38 +94,39 @@ bundle
        Sequence s
     -> Sequence t
     -> Sequence (s, t)
-bundle left right = Sequence $ do
-    firstl :: s <- sequenceFirst left
-    firstr :: t <- sequenceFirst right
-    restl :: Event s <- sequenceRest left
-    restr :: Event t <- sequenceRest right
-    bl :: Behavior s <- stepper firstl restl
-    br :: Behavior t <- stepper firstr restr
-    -- Now we create an event which merges s and t, handling the case in which
-    -- they fire simultaneously by using the EitherBoth type.
-    let evLDiscriminated :: Event (EitherBoth s t)
-        evLDiscriminated = OneLeft <$> restl
-    let evRDiscriminated :: Event (EitherBoth s t)
-        evRDiscriminated = OneRight <$> restr
-    let evLR :: Event (EitherBoth s t)
-        evLR = unionWith (\(OneLeft s) (OneRight t) -> Both s t)
-                         (evLDiscriminated)
-                         (evRDiscriminated)
-    -- We throw in the behaviors for s and t, yielding an event which contains
-    -- all the information we need in order to decide when to fire.
-    let evLR' :: Event ((s, t), EitherBoth s t)
-        evLR' = (,) <$> ((,) <$> bl <*> br) <@> evLR
-    let pick :: ((s, t), EitherBoth s t) -> Maybe (s, t)
-        pick x = case x of
-            -- If both fire, just give them.
-            (_, Both s t) -> Just (s, t)
-            -- If only one fires, give the latest tuple.
-            ((_, t), OneLeft s) -> Just (s, t)
-            ((s, _), OneRight t) -> Just (s, t)
-    return ((firstl, firstr), filterJust (pick <$> evLR'))
-
-always :: t -> Sequence t
-always x = x |> never
+bundle left right = Sequence content id
+  where
+    content = do
+        firstl :: s <- sequenceFirst left
+        firstr :: t <- sequenceFirst right
+        let theRest = do
+                restl :: Event s <- sequenceRest left
+                restr :: Event t <- sequenceRest right
+                bl :: Behavior s <- stepper firstl restl
+                br :: Behavior t <- stepper firstr restr
+                -- Now we create an event which merges s and t, handling the case in which
+                -- they fire simultaneously by using the EitherBoth type.
+                let evLDiscriminated :: Event (EitherBoth s t)
+                    evLDiscriminated = OneLeft <$> restl
+                let evRDiscriminated :: Event (EitherBoth s t)
+                    evRDiscriminated = OneRight <$> restr
+                let evLR :: Event (EitherBoth s t)
+                    evLR = unionWith (\(OneLeft s) (OneRight t) -> Both s t)
+                                     (evLDiscriminated)
+                                     (evRDiscriminated)
+                -- We throw in the behaviors for s and t, yielding an event which contains
+                -- all the information we need in order to decide when to fire.
+                let evLR' :: Event ((s, t), EitherBoth s t)
+                    evLR' = (,) <$> ((,) <$> bl <*> br) <@> evLR
+                let pick :: ((s, t), EitherBoth s t) -> Maybe (s, t)
+                    pick x = case x of
+                        -- If both fire, just give them.
+                        (_, Both s t) -> Just (s, t)
+                        -- If only one fires, give the latest tuple.
+                        ((_, t), OneLeft s) -> Just (s, t)
+                        ((s, _), OneRight t) -> Just (s, t)
+                return (filterJust (pick <$> evLR'))
+        return ((firstl, firstr), theRest)
 
 fromEvent :: Event t -> Sequence (Maybe t)
 fromEvent event = Nothing |> (Just <$> event)
@@ -132,28 +145,37 @@ toBehavior sequence = do
 -- | Give an initial value and an event producing changes, and get a sequence
 --   whose values are applications of those changes.
 fromDeltas :: t -> Event (t -> t) -> Sequence t
-fromDeltas x xs = Sequence $ mdo
-    let event = (flip ($)) <$> behavior <@> xs
-    behavior <- stepper x event
-    return (x, event)
+fromDeltas x xs = Sequence content id
+  where
+    content = mdo
+        let event = (flip ($)) <$> behavior <@> xs
+        behavior <- stepper x event
+        return (x, return event)
 
 sequenceUnion :: (s -> s -> s) -> Sequence s -> Sequence s -> Sequence s
-sequenceUnion unioner left right = Sequence $ do
-    firstl <- sequenceFirst left
-    firstr <- sequenceFirst right
-    restl <- sequenceRest left
-    restr <- sequenceRest right
-    return (unioner firstl firstr, unionWith unioner restl restr)
+sequenceUnion unioner left right = Sequence content id
+  where
+    content = do
+        firstl <- sequenceFirst left
+        firstr <- sequenceFirst right
+        let theRest = do
+                restl <- sequenceRest left
+                restr <- sequenceRest right
+                return (unionWith unioner restl restr)
+        return (unioner firstl firstr, theRest)
 
 sequenceChoice :: (Semigroup s) => Sequence s -> Sequence s -> Sequence s
-sequenceChoice left right = Sequence $ do
-    firstl <- sequenceFirst left
-    firstr <- sequenceFirst right
-    let first = firstl <> firstr
-    restl <- sequenceRest left
-    restr <- sequenceRest right
-    let rest = unionWith (<>) restl restr
-    return (first, rest)
+sequenceChoice left right = Sequence content id
+  where
+    content = do
+        firstl <- sequenceFirst left
+        firstr <- sequenceFirst right
+        let first = firstl <> firstr
+        let theRest = do
+                restl <- sequenceRest left
+                restr <- sequenceRest right
+                return (unionWith (<>) restl restr)
+        return (first, theRest)
 
 instance Semigroup s => Semigroup (Sequence s) where
     (<>) = sequenceChoice
@@ -163,21 +185,23 @@ instance (Semigroup s, Monoid s) => Monoid (Sequence s) where
     mappend = (<>)
 
 sequenceUnion' :: (s -> s -> s) -> Sequence s -> Event s -> Sequence s
-sequenceUnion' unioner left right = Sequence $ do
-    firstl <- sequenceFirst left
-    restl <- sequenceRest left
-    return (firstl, unionWith unioner restl right)
+sequenceUnion' unioner left right = Sequence content id
+  where
+    content = do
+        firstl <- sequenceFirst left
+        restl <- sequenceRest left
+        let theRest = return (unionWith unioner restl right)
+        return (firstl, theRest)
 
 -- | Commute Sequence and MomentIO.
 sequenceCommute :: Sequence (MomentIO t) -> MomentIO (Sequence t)
 sequenceCommute sequence = do
-    first :: MomentIO t <- sequenceFirst sequence
-    rest :: Event (MomentIO t) <- sequenceRest sequence
-    first' <- first
-    rest' <- execute rest
-    let mkSequence :: MomentIO (t, Event t)
-        mkSequence = return (first', rest')
-    return (Sequence mkSequence)
+    let content = do first <- sequenceFirst sequence
+                     first' <- first
+                     let theRest = do rest <- sequenceRest sequence
+                                      execute rest
+                     return (first', theRest)
+    return (Sequence content id)
 
 {-
 sequenceExecute' :: Sequence (MomentIO t) -> Sequence t
@@ -198,6 +222,37 @@ sequenceExecute'' sequence = Sequence $ do
     return (first', rest'')
 -}
 
+-- We've got 
+--
+--   MomentIO (Event t)
+--   MomentIO (Event (Event t))
+--
+-- How can we produce MomentIO (Event t) while being maximally lazy?
+-- I think we just have to force them...
+--
+-- What if instead we gave
+--
+--     Sequence (Event t) -> MomentIO (Event (MomentIO t))
+--
+-- ??
+--
+-- Imagine if
+--
+--     type Sequence t = forall s . (s -> t) -> (MomentIO (s, MomentIO (Event (MomentIO s))))
+--
+-- Then could we do sequenceSwitch more lazily?
+--
+--     sequenceSwitch sequence = do
+--         first Event t <- sequenceFirst sequence
+--         rest :: Event (MomentIO (Event t)) <- sequenceRest sequence
+--         rests :: Event (Event t) <- execute rest
+--         return $ unionWith const first rests
+--
+-- We have to force the initial, and the Event
+--
+-- | This will force both parts of your sequence: the initial value and
+--   the subsequent event. Be careful when using this with recursive do
+--   notation.
 sequenceSwitch :: forall t . Sequence (Event t) -> MomentIO (Event t)
 sequenceSwitch sequence = do
     first :: Event t <- sequenceFirst sequence
