4ef197b16c56a440fb9534990acae43ebdeab906
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2016-03-17 09:59:10 -0400

    XHR handlers via arrows
    TODO make a richer dsl so that we can do parallel xhrs

--- Reactive/DOM/XHR.hs
+++ Reactive/DOM/XHR.hs
@@ -16,12 +16,19 @@ Portability : non-portable (GHC only)
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE Arrows #-}
 
 module Reactive.DOM.XHR (
 
       XHRHandler
     , xhrHandler
-    , xhrHandlerSum
+
+    {-
+    , XHRFolder
+    , xhrFolder
+    , xhrFolderTrivial
+    -}
+
     , xhr1
     , xhrMany
     , xhrLimited
@@ -35,6 +42,8 @@ module Reactive.DOM.XHR (
 
     ) where
 
+import Prelude hiding ((.), id)
+import Control.Category
 import Control.Arrow
 import Control.Monad (forM_, when)
 import Control.Monad.Trans.Reader (ask)
@@ -98,38 +107,140 @@ data XHRResponse = XHRResponse {
 
 deriving instance Show XHRResponse
 
+-- Want to be able to make a kind of XHR handler fold, in which subsequent
+-- requests may use data returned from prior responses.
+-- Well, if we deal with XHRHandler s s then 
+
+-- | Given an input s, make an XHRRequest and a function to handle the
+--   XHRResponse. It also allows pure functions, making it a Category and an
+--   Arrow.
 newtype XHRHandler s t = XHRHandler {
-      runXHRHandler :: s -> (XHRRequest, XHRResponse -> t)
+      runXHRHandler :: s -> Either t (XHRRequest, XHRHandler XHRResponse t)
     }
 
 instance Profunctor XHRHandler where
-    dimap l r (XHRHandler f) = XHRHandler $ \s ->
-        let (req, mkRes) = f (l s)
-        in  (req, fmap r mkRes)
+    dimap l r (XHRHandler f) = XHRHandler $ \s -> case f (l s) of
+        Left x -> Left (r x)
+        Right (req, res) -> Right (req, rmap r res)
+
+-- | Category composition of an XHRHandlers means do the left after the
+--   right.
+instance Category XHRHandler where
+    id = XHRHandler Left
+    left . right = XHRHandler $ \s -> case runXHRHandler right s of
+        Left t -> runXHRHandler left t
+        Right (req, res) -> Right (req, left . res)
+
+instance Arrow XHRHandler where
+    arr f = XHRHandler (Left . f)
+    first (XHRHandler f) = XHRHandler $ \(s, c) -> case f s of
+        Left t -> Left (t, c)
+        Right (req, res) -> Right (req, arr (\x -> (x, c)) . res)
+
+-- | Whereas Category composition will sequence requests, ArrowChoice
+--   disjunction will choose between requests.
+instance ArrowChoice XHRHandler where
+    left (XHRHandler f) = XHRHandler $ \choice -> case choice of
+        Left x -> case f x of
+            Left y -> Left (Left y)
+            Right (req, res) -> Right (req, arr Left . res)
+        Right y -> Left (Right y)
+
+instance ArrowApply XHRHandler where
+    app = XHRHandler $ \(handler, input) -> runXHRHandler handler input
 
 xhrHandler :: (s -> (XHRRequest, XHRResponse -> t)) -> XHRHandler s t
-xhrHandler = XHRHandler
+xhrHandler f = XHRHandler $ \s ->
+    let (req, resToT) = f s
+        res = XHRHandler $ \r -> Left (resToT r)
+    in  Right (req, res)
+    
+newtype XhrContinuation t = XhrContinuation {
+      getXhrContinuation :: Either t (XMLHttpRequest, Banana.Event (XhrContinuation t))
+    }
 
--- | Now suppose we have two XHRHandlers and we want to combine them?
---        XHRHandler sL tL
---     -> XHRHandler sR tR
---     -> XHRHandler (Either sL sR) (Either tL tR)
---   This is like +++ from Arrow, but of course XHRHandler is not even
---   a category.
---   So maybe there's no typeclass for this, but here it is anyway.
-xhrHandlerSum
-    :: XHRHandler sL tL
-    -> XHRHandler sR tR
-    -> XHRHandler (Either sL sR) (Either tL tR)
-xhrHandlerSum (XHRHandler l) (XHRHandler r) = XHRHandler $ \choice -> case choice of
-    Left sL -> (fmap . fmap) Left (l sL)
-    Right sR -> (fmap . fmap) Right (r sR)
+runXhrContinuation
+    :: forall t .
+       XhrContinuation t
+    -> Either t (XMLHttpRequest, (Banana.Event (Either XMLHttpRequest t)))
+runXhrContinuation xhrcont = case getXhrContinuation xhrcont of
+    Left t -> Left t
+    Right (xhr, rest) -> 
+        let stripped :: Banana.Event (Either t (XMLHttpRequest, Banana.Event (Either XMLHttpRequest t)))
+            stripped = runXhrContinuation <$> rest
+            -- immediate and delayed are mutually exclusive. If immediate fires,
+            -- delayed is never.
+            immediate :: Banana.Event t
+            immediate = filterJust (either Just (const Nothing) <$> stripped)
+            delayed :: Banana.Event (XMLHttpRequest, Banana.Event (Either XMLHttpRequest t))
+            delayed = filterJust (either (const Nothing) Just <$> stripped)
+            switched :: Banana.Event (Either XMLHttpRequest t)
+            switched = switchE (snd <$> delayed)
+            firstXhr :: Banana.Event XMLHttpRequest
+            firstXhr = fst <$> delayed
+            unioned = unionWith const (Right <$> immediate)
+                      (unionWith const (Left <$> firstXhr) switched)
+        in  Right (xhr, unioned)
 
-xhr1 :: XHRHandler s t -> s -> Compose MomentIO Banana.Event t
-xhr1 handler s = Compose $ do
-    let (request, mkResponse) = runXHRHandler handler s
-    (ev, _) <- makeXHRFromRequest request
-    pure (mkResponse <$> ev)
+evalXhrHandler
+    :: forall s t .
+       XHRHandler s t
+    -> s
+    -> MomentIO (XhrContinuation t)
+evalXhrHandler (XHRHandler f) s = case f s of
+    Left t -> pure . XhrContinuation . Left $ t
+    Right (req, res) -> do
+        -- Spawn this request right away.
+        (ev, xhr) :: (Banana.Event XHRResponse, XMLHttpRequest)
+            <- makeXHRFromRequest req
+        -- Whenever the response comes, run the response handler on it to
+        -- yield a continuation.
+        next :: Banana.Event (XhrContinuation t)
+            <- execute (evalXhrHandler res <$> ev)
+        pure . XhrContinuation . Right $ (xhr, next)
+
+{-
+-- | An XHRHandler which may depend upon the output type.
+--   These are used by xhrLimited, which feeds the latest response back into
+--   the folder so that subsequent requests may depend upon it.
+data XHRFolder s t where
+    XHRFolder :: XHRHandler r q -> (Maybe q -> s -> Maybe r) -> (q -> t) -> XHRFolder s t
+
+instance Profunctor XHRFolder where
+    dimap l r (XHRFolder h f g) = XHRFolder h f' g'
+      where
+        g' = fmap r g
+        f' x s = f x (l s)
+
+runXHRFolder :: XHRFolder s t -> Maybe t -> s -> Maybe (XHRHandler () t)
+runXHRFolder (XHRFolder h f g) maybet s = case f maybet s of
+    Nothing -> Nothing
+    Just r -> Just (arr (const r) >>> h >>> arr g)
+
+{-
+runXHRFolder' :: XHRFolder s t -> XHRHandler (Maybe t, s) (Maybe t)
+runXHRFolder' (XHRFolder h f) = proc (maybet, s) -> do
+    case f maybet s of
+        Nothing -> do returnA -< Nothing
+        Just r -> do
+            t <- h -< r
+            returnA -< Just t
+-}
+
+xhrFolder :: (Maybe t -> s -> Maybe r) -> XHRHandler r t -> XHRFolder s t
+xhrFolder f h = XHRFolder h f id
+
+xhrFolderTrivial :: XHRHandler s t -> XHRFolder s t
+xhrFolderTrivial h = XHRFolder h (const Just) id
+-}
+
+-- | Run an XHRHandler immediately. 
+xhr1 :: XHRHandler s t -> s -> MomentIO (Eventually t)
+xhr1 handler s = do
+    cont <- evalXhrHandler handler s
+    case runXhrContinuation cont of
+        Left t -> pure (immediately t)
+        Right (_, ev) -> pure (delayed (filterJust (either (const Nothing) Just <$> ev)))
 
 -- | For every occurrence of an event, use an XHRHandler to launch a request
 --   and handle it. If the event fires before a response is heard, the response
@@ -143,7 +254,62 @@ xhrMany
     -> Banana.Event s
     -> Compose MomentIO Banana.Event t
 xhrMany handler sev = Compose $ mdo
-    let requestsAndResponders = runXHRHandler handler <$> sev
+
+    continuations :: Banana.Event (Either t (XMLHttpRequest, (Banana.Event (Either XMLHttpRequest t))))
+        <- execute ((fmap . fmap) runXhrContinuation (evalXhrHandlerAndCancel handler <$> currentXhr <@> sev))
+
+    let immediates :: Banana.Event t
+        immediates = filterJust (either Just (const Nothing) <$> continuations)
+
+    let delayeds :: Banana.Event (Either XMLHttpRequest t)
+        delayeds = switchE (snd <$> (filterJust (either (const Nothing) Just <$> continuations)))
+
+    let firstXhr = fst <$> (filterJust (either (const Nothing) Just <$> continuations))
+    let delayedValues = filterJust (either (const Nothing) Just <$> delayeds)
+    let delayedXhrs = filterJust (either Just (const Nothing) <$> delayeds)
+
+    -- Xhr changes to a Just whenever a new one appears from delayeds, and
+    -- to Nothing whenever we actually hear a response (I assume, perhaps
+    -- incorrectly, that if we abort() an Xhr, its readystate callback will
+    -- not fire).
+    let setXhr :: Banana.Event (Maybe XMLHttpRequest)
+        setXhr = Just <$> (unionWith const delayedXhrs firstXhr)
+    let unsetXhr :: Banana.Event (Maybe XMLHttpRequest)
+        unsetXhr = const Nothing <$> delayedValues
+    let changeXhr = unionWith const setXhr unsetXhr
+    reactimate (const (Prelude.print "set") <$> setXhr)
+    reactimate (const (Prelude.print "unset") <$> unsetXhr)
+    currentXhr :: Behavior (Maybe XMLHttpRequest)
+        <- stepper Nothing changeXhr
+
+    pure (unionWith const immediates delayedValues)
+
+  where
+
+    evalXhrHandlerAndCancel
+        :: XHRHandler s t
+        -> Maybe XMLHttpRequest
+        -> s
+        -> MomentIO (XhrContinuation t)
+    evalXhrHandlerAndCancel handler maybeXhr s = do
+        cancelInFlight maybeXhr
+        evalXhrHandler handler s
+
+    cancelInFlight :: Maybe XMLHttpRequest -> MomentIO ()
+    cancelInFlight in_xhr = case in_xhr of
+        Nothing -> do
+            liftIO (putStrLn "cancelInFlight : Nothing")
+            pure ()
+        Just in_flight -> do
+            liftIO (putStrLn "cancelInFlight : Just")
+            liftIO (XHR.abort in_flight)
+            --readyState <- liftIO (getReadyState in_flight)
+            --if readyState /= 4
+            --then liftIO (XHR.abort in_flight)
+            --else pure ()
+
+
+{-
     let requests :: Banana.Event XHRRequest
         requests = fst <$> requestsAndResponders
     let responders :: Banana.Event (XHRResponse -> t)
@@ -176,14 +342,6 @@ xhrMany handler sev = Compose $ mdo
     spawnRequest inflight req = do
         liftIO (cancelInFlight inflight)
         makeXHRFromRequest req
-    cancelInFlight :: Maybe XMLHttpRequest -> IO ()
-    cancelInFlight in_xhr = case in_xhr of
-        Nothing -> pure ()
-        Just in_flight -> do
-            readyState <- getReadyState in_flight
-            if readyState /= 4
-            then XHR.abort in_flight
-            else pure ()
 
 applyCurrentResponder
     :: forall t .
@@ -192,6 +350,7 @@ applyCurrentResponder
     -> Maybe t
 applyCurrentResponder Nothing _ = trace "Got response but have no responder. This is a bug." Nothing
 applyCurrentResponder (Just f) res = Just (f res)
+-}
 
 -- | Spawn an XHR for every XHRRequest in the event which occurrs when there is
 --   no other XHR spawned as a result of that event. If the event fires multiple
@@ -203,15 +362,81 @@ applyCurrentResponder (Just f) res = Just (f res)
 --   (PUT/POST rather than GET) because aborting such a request does not
 --   guarantee that the effects won't be realized.
 --
+
 xhrLimited
-    :: forall s t .
-       XHRHandler s t
+    :: forall r s t .
+       ( Monoid r )
+    => XHRHandler (r, s) (r, Maybe t)
     -> Banana.Event s
     -> Compose MomentIO Banana.Event t
 xhrLimited handler sev = Compose $ mdo
 
-    let requestsAndResponders :: Banana.Event (XHRRequest, XHRResponse -> t)
-        requestsAndResponders = runXHRHandler handler <$> sev
+    -- TBD may be useful to log whenever a Nothing appears?
+
+    let runIt :: r -> s -> XHRHandler () (r, Maybe t)
+        runIt r s = lmap (const (r, s)) handler
+    let handlers :: Banana.Event (XHRHandler () (r, Maybe t))
+        handlers = runIt <$> mostRecentEvaluatedResponse <@> sev
+
+    continuations :: Banana.Event (Either (r, Maybe t) (XMLHttpRequest, Banana.Event (Either XMLHttpRequest (r, Maybe t))))
+        <- execute ((fmap . fmap) runXhrContinuation (flip evalXhrHandler () <$> willSpawn))
+
+    -- We identify the handlers which fire while there's no in-flight xhr,
+    -- and those which fire when there is an in-flight xhr.
+    let passed :: Banana.Event (XHRHandler () (r, Maybe t))
+        passed = whenE pass handlers
+    let held :: Banana.Event (XHRHandler () (r, Maybe t))
+        held = whenE hold handlers
+    -- Held handlers must be enqueued so that we can run them when the in-flight
+    -- returns.
+    let changeWaiting :: Banana.Event (Maybe (XHRHandler () (r, Maybe t)))
+        changeWaiting = unionWith const (Just <$> held) (const Nothing <$> delayedValues)
+    waiting :: Behavior (Maybe (XHRHandler () (r, Maybe t)))
+        <- stepper Nothing changeWaiting
+
+    -- Fire the queued thing whenever delayed values comes back (signals the
+    -- end of whatever xhr is out there).
+    let queued :: Banana.Event (XHRHandler () (r, Maybe t))
+        queued = filterJust (waiting <@ delayedValues)
+
+    let willSpawn :: Banana.Event (XHRHandler () (r, Maybe t))
+        willSpawn = unionWith const passed queued
+
+    let immediates :: Banana.Event (r, Maybe t)
+        immediates = filterJust (either Just (const Nothing) <$> continuations)
+
+    let delayeds :: Banana.Event (Either XMLHttpRequest (r, Maybe t))
+        delayeds = switchE (snd <$> (filterJust (either (const Nothing) Just <$> continuations)))
+
+    -- delayedValues indicates not only the value, but also the end of an xhr,
+    -- so it's an important control mechanism.
+    let firstXhr = fst <$> (filterJust (either (const Nothing) Just <$> continuations))
+    let delayedValues = filterJust (either (const Nothing) Just <$> delayeds)
+    let delayedXhrs = filterJust (either Just (const Nothing) <$> delayeds)
+
+    -- Xhr changes to a Just whenever a new one appears from delayeds, and
+    -- to Nothing whenever we actually hear a response (I assume, perhaps
+    -- incorrectly, that if we abort() an Xhr, its readystate callback will
+    -- not fire).
+    let setXhr :: Banana.Event (Maybe XMLHttpRequest)
+        setXhr = Just <$> (unionWith const delayedXhrs firstXhr)
+    let unsetXhr :: Banana.Event (Maybe XMLHttpRequest)
+        unsetXhr = const Nothing <$> delayedValues
+    let changeXhr = unionWith const setXhr unsetXhr
+    currentXhr :: Behavior (Maybe XMLHttpRequest)
+        <- stepper Nothing changeXhr
+
+    let hold :: Behavior Bool
+        hold = isJust <$> currentXhr
+    let pass :: Behavior Bool
+        pass = isNothing <$> currentXhr
+
+    mostRecentEvaluatedResponse :: Behavior r
+        <- stepper mempty (fst <$> delayedValues)
+
+    pure (filterJust (snd <$> unionWith const immediates delayedValues))
+
+{-
 
     -- We have at most one queued request (input event fires while a request
     -- was in flight). Whenever we spawn a request we set it to Nothing.
@@ -249,8 +474,18 @@ xhrLimited handler sev = Compose $ mdo
         spawns = makeXHRFromRequest . fst <$> spawnOne
     outs :: Banana.Event (Banana.Event XHRResponse, XMLHttpRequest)
         <- execute spawns
-    let responses = switchE (fst <$> outs)
-    pure (filterJust (applyCurrentResponder <$> currentResponder <@> responses))
+    let responses :: Banana.Event XHRResponse
+        responses = switchE (fst <$> outs)
+    let evaluatedResponses :: Banana.Event (Maybe t)
+        evaluatedResponses = applyCurrentResponder <$> currentResponder <@> responses
+
+    -- We track the last evaluated response so that we can feed it back into
+    -- the next request. 
+    mostRecentEvaluatedResponse :: Behavior (Maybe t)
+        <- stepper Nothing evaluatedResponses
+
+    pure (filterJust evaluatedResponses)
+-}
 
 -- | Make and send an XHR. You get the event giving the response, and the
 --   XHR itself, in case perhaps you want to cancel it.
